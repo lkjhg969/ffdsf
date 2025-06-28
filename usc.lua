1fd73506badeae698981437c4c40d32f5b8fe2ea
lkjhg969 <tvhennezockt@gmail.com>
2025-06-28 21:44:03 +0200

    Update usc.lua

--- usc.lua
+++ usc.lua
@@ -1,1 +1,9 @@
-
+return {
+    ["vloraharam"] = Color3.fromRGB(138, 0, 0),
+    ["HenneZocktTV"] = Color3.fromRGB(138, 0, 0),
+    ["indieuns_fan"] = Color3.fromRGB(138, 0, 0),
+    ["Dino112359"] = Color3.fromRGB(138, 0, 0),
+    ["HENNExDINO"] = Color3.fromRGB(138, 0, 0),
+    [""] = Color3.fromRGB(0, 0, 0),
+    [""] = Color3.fromRGB(0, 0, 0),
+}
